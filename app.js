8b3126419e70a6b7f1c3dfe7e91f24d7c270f6b7
connorHashDash <ckb1997@hotmail.co.uk>
2022-03-26 22:08:59 +0000

    can now alternate moves, two player technically

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-// Make an object constructor when the cells are being created.
-// Program a computer and player controlled state of the cells which is triggered upon clicking
-// The cell or the computer taking it.
 const log = console.log;
 // Variable Declarations
 let boardDiv = document.getElementById('gameBoard');
 //The Game board is stored in this array
 let cellArray = [];
+    let token = 'x'
 let game = (() => {
   const cellMaker = ((row) => {
 
   let cellFactory = (row, col) => {
-    let nought = false;
-    let cross = true;
     let cell = document.createElement('div');
+    let played = false;
     const create = (row, col) => {
       cell.className = `${row}${col} cell`;
       boardDiv.appendChild(cell);
-      cell.addEventListener('click', function() {
-        let cross = document.createElement('p')
-        cross.innerHTML='x'
-        cell.appendChild(cross)
-      })
+      cell.addEventListener('click', function() {play();}) // you need to call a click function like this
     }
     const play = () => { 
+      if (played == false) {
+        let cross = document.createElement('p');
+        cross.innerHTML=`${token}`;
+        cross.className ='contents';
+        cell.appendChild(cross);
+        played = true;
+        if (token == 'x'){token = 'o'; return}
+        else {token = 'x'}
+      }
     };
-
-    return {row, col, create, play}
+    return {row, col, create}
   }
-
     let j = 0
   for (let i = 0; i < 9; i++) { //This creates the gameboard and assigns the cells names
     if (j > 2) {j = 0}
@@ -45,6 +45,5 @@ let game = (() => {
      j++ 
     }
   }
-
   })()
 })()
